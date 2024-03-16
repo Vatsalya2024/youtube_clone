dd34710fddbb95eea63756f2c292b1137a7cfd78
Vatsalya Singh Baghel <vatsalyasinghbaghel@Vatsalyas-MacBook-Air.local>
2024-03-16 12:04:14 +0530

    firebase setup,header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC29arMj0YwpBuOnU9BXO-2d10byFUdTxs",
+  authDomain: "clone-545cb.firebaseapp.com",
+  projectId: "clone-545cb",
+  storageBucket: "clone-545cb.appspot.com",
+  messagingSenderId: "904791747936",
+  appId: "1:904791747936:web:e24eb7f5424747c2568205",
+  measurementId: "G-V7K0EQK5R3",
+};
